e240adac00fddd99de75ca296358d6e41918c699
Phoboros <90618896+Phoboros@users.noreply.github.com>
2022-05-22 22:15:48 +0200

    Version experimentale
    Ajoute des fonctions pour la récupération de w et b, de la fonction effectuant l'operation matricielle etudiee, de la fonction trouvant l'indice du maximum de correspondance (dans le résultat de l'opération matricielle susmentionnée).

--- outils_maths.c
+++ outils_maths.c
@@ -28,3 +28,101 @@ double* mu_sigma(double* t, int n_col, int n_lignes){
 
 }
 
+
+
+
+void read_array(char filename[], int n, double arr[][n])
+{
+    FILE* fp = fopen(filename, "r");
+    //double arr[10][514];
+    if (!fp)
+        printf("Can't open file\n");
+ 
+    else {
+        char buffer[4096];
+ 
+        int row = 0;
+        int column = 0;
+ 
+        while (fgets(buffer,
+                     4096, fp)) {
+            column = 0;
+            char* value = strtok(buffer, ", ");
+            while (value) {
+                arr[row][column] = atof(value);
+                value = strtok(NULL, ", ");
+                column++;
+            }
+            row++;
+        }
+        fclose(fp);
+    }
+}
+
+
+void read_vector(char filename[], int n, double* arr)
+{
+    FILE* fp = fopen(filename, "r");
+    if (!fp)
+        printf("Can't open file\n");
+ 
+    else {
+        char buffer[4096];
+ 
+        int row = 0;
+        int column = 0;
+ 
+        while (fgets(buffer,
+                     4096, fp)) {
+            column = 0;
+            char* value = strtok(buffer, ", ");
+            while (value) {
+                arr[row][column] = atof(value);
+                value = strtok(NULL, ", ");
+                column++;
+            }
+            row++;
+        }
+        fclose(fp);
+    }
+}
+
+
+
+void operation_matricielle(int n, int p ,double w[][p], double* b, double* x, double* res) {
+	/* n est le nombre de genres musicaux, donc de lignes de la matrice ; c'est donc également la taille de b et de res */
+	/* p est le nombre d'échantillons, donc de colonnes de la matrice ; c'est également la taille de x */
+	
+	double somme_sur_j;
+	int i,j;
+	
+	for(i=0; i<n; i++) {
+	
+		somme_sur_j = b[i];
+		
+		for(j=0; j<p; j++) {
+		
+			somme_sur_j = somme_sur_j + ((w[i][j]) * (x[j]));
+		
+			}
+	
+		res[i] = somme_sur_j;
+		
+	}
+	
+}
+
+int indice_maximum(double* l, int n) {
+
+	int i_max = 0;
+	int k;
+	
+	for (k=1; k<n; k++) {
+		if (l[k] > l[i_max]) {
+			i_max = k;
+		}
+	}
+	
+	return i_max;
+	
+}
