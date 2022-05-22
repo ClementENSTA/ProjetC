5660225fb94757c8cdbb1e8b208b0549f0f95cc2
Phoboros <90618896+Phoboros@users.noreply.github.com>
2022-05-22 22:16:29 +0200

    Version experimentale

--- outils_maths.h
+++ outils_maths.h
@@ -1,1 +1,9 @@
 double* mu_sigma(double* t, int n_col, int n_lignes);
+
+void read_array(char filename[], int n, double arr[][n]);
+
+void read_vector(char filename[], int n, double* arr);
+
+void operation_matricielle(int n, int p ,double w[][p], double* b, double* x, double* res);
+
+int indice_maximum(double* l, int n);
